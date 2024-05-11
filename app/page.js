5378334d5a02a567991e0c9e13639f2cde41fe57
Fareen Khan <kfareen34@gmail.com>
2024-05-11 13:21:14 -0400

    General Theme and Formatting Change

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default async function Home({ searchParams }) {
   const data = recipesData; 
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-neutral-900">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-sans text-sm">
         <h1 className="text-4xl p-4 text-center">Recipe Finder</h1>
         <Search />
--- components/List.js
+++ components/List.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function List({ Ingredients }) {
   return (
     <div className="divide-y divide-slate-400 divide-opacity-30">
-      <h1 className="text-lg pb-1">Ingredients</h1>
+      <h1 className="text-lg pb-1 font-semibold">Ingredients</h1>
       <ul className="mb-3 font-normal pt-2 px-5 list-disc">
         {Ingredients.map((ingredient, index) => (
           <li key={index}>{ingredient}</li>
